dd418d613823d067559e1e96099164a07d395e3c
Vinh Tran <vnntran@ucdavis.edu>
2018-01-21 14:25:08 -0800

    added comments to api request
    added comments to api request

--- AirPollution/routes/index.js
+++ AirPollution/routes/index.js
@@ -6,9 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pollution Levels Tracker' });
 });
 
+/////////////// API request for website http://aqicn.org/api/ /////////////////////////////////////////
 var request = require('request');
 var cheerio = require('cheerio');
-
 request('https://api.waqi.info/search/?token=demo&keyword=bangalore', function(error, response, body){
     if(!error && response.statusCode == 200){
         var $ = cheerio.load(body);
@@ -17,8 +17,10 @@ request('https://api.waqi.info/search/?token=demo&keyword=bangalore', function(e
         var json = body;
         console.log(json);
         obj = JSON.parse(json);
+        var apiVar = obj.data[0].aqi; //aqi data is at array 0 of data in JSON
         console.log(obj.data[0].aqi);
+
     }
 });
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = router;
