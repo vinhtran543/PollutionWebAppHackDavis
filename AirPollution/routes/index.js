82606b527bf58203f7fb49aa6a48b4dca5fff657
Vinh Tran <vnntran@ucdavis.edu>
2018-01-21 13:07:12 -0800

    created a working parser
    created a working parser  to grab aqi (air quality information) from the
    website url

--- AirPollution/routes/index.js
+++ AirPollution/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pollution Levels Tracker' });
 });
 
+var request = require('request');
+var cheerio = require('cheerio');
+
+request('https://api.waqi.info/search/?token=demo&keyword=bangalore', function(error, response, body){
+    if(!error && response.statusCode == 200){
+        var $ = cheerio.load(body);
+        //console.log(body);
+
+        var json = body;
+        console.log(json);
+        obj = JSON.parse(json);
+        console.log(obj.data[0].aqi);
+    }
+});
+
 module.exports = router;
