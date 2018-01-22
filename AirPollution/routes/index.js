8a04a28bb4b02cab1fe069c196dc7599c4bd7ba7
Vinh Tran <vnntran@ucdavis.edu>
2018-01-21 20:53:41 -0800

    Added working bar graph still needs work
    added working bar graph still needs work + clean up + need to find city
    dynamically

--- AirPollution/routes/index.js
+++ AirPollution/routes/index.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pollution Levels Tracker' });
+  res.render('index', { title: 'Pollution Levels Tracker', aqiEJStoHtml: globalaqi, barHeight: globalbarHeight });
 });
+
 
 /////////////// API request for website http://aqicn.org/api/ /////////////////////////////////////////
 var request = require('request');
 var cheerio = require('cheerio');
+global.globalaqi = "";
+globalbarHeight = "";
+
 request('https://api.waqi.info/search/?token=demo&keyword=bangalore', function(error, response, body){
     if(!error && response.statusCode == 200){
         var $ = cheerio.load(body);
@@ -17,10 +21,18 @@ request('https://api.waqi.info/search/?token=demo&keyword=bangalore', function(e
         var json = body;
         console.log(json);
         obj = JSON.parse(json);
-        var apiVar = obj.data[0].aqi; //aqi data is at array 0 of data in JSON
-        console.log(obj.data[0].aqi);
+        var aqiVar = obj.data[0].aqi; //aqi data is at array 0 of data in JSON
+        //console.log(obj.data[0].aqi);
+        console.log(aqiVar);
+        globalaqi = aqiVar;
+        globalbarHeight = Math.round(aqiVar / 3); //divide by 3 since scale is 300% not 100% for bar graph
 
+        // globalbarHeight = globalaqi / 2;
+        // console.log("barheight: " + barHeight);
     }
 });
+
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = router;
