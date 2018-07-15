167aa6c94df28493ea10c4ae4e5343ba284a9e30
Vinh Tran <vnntran@ucdavis.edu>
2018-07-14 21:57:49 -0700

    Cleaned out old code, put in comments
    Cleaned out old code that were useless and were originally commented
    out, put in comments

--- AirPollution/routes/index.js
+++ AirPollution/routes/index.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
-search = "";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pollution Levels Tracker', aqiEJStoHtml: globalaqi, barHeight: globalbarHeight });
 });
 
-//var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
@@ -15,73 +13,18 @@ var app = express();
 //Note that in version 4 of express, express.bodyParser() was
 //deprecated in favor of a separate 'body-parser' module.
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(express.bodyParser());
 app.post('/myaction', function(req, res) {
-    //res.send('You sent the name "' + req.body.username + '".');
     keyword = req.body.username;
     console.log("In the body of app.post()");
-    console.log("search: " + keyword);
-    //createbargraph(search);
+    console.log("keyword search: " + keyword);
+
+    //function call to createbargraph graph and refresh page with changes
     app.get(createbargraph(keyword, req, res));
-    // request('https://api.waqi.info/search/?token=5e3430ee03115b533faddd23e524e086e4915a74&keyword=' + keyword, function(error, response, body){
-    //     if(!error && response.statusCode == 200){
-    //         var $ = cheerio.load(body);
-    //         //console.log(body);
-    //
-    //         var json = body;
-    //         console.log(json);
-    //         obj = JSON.parse(json);
-    //         var aqiVar = obj.data[0].aqi; //aqi data is at array 0 of data in JSON
-    //         //console.log(obj.data[0].aqi);
-    //         console.log(aqiVar);
-    //         globalaqi = aqiVar;
-    //         globalbarHeight = Math.round(aqiVar / 3); //divide by 3 since scale is 300% not 100% for bar graph
-    //
-    //         // globalbarHeight = globalaqi / 2;
-    //         // console.log("barheight: " + barHeight);
-    //         res.redirect("http://localhost:3000/");
-    //     }
-    // });
-
-
 });
 
 app.listen(8080, function() {
     console.log('Server running at http://127.0.0.1:8080/');
 });
-
-// var app = express();
-// search = "";
-// console.log("HERE");
-// app.get('/', function(req, res) {
-//     console.log("HERE");
-//     res.send('Username: ' + req.query['username']);
-//     search = req.query.username;
-//     console.log(search);
-// });
-// console.log("HERE");
-
-//Request object from html page (index.ejs)
-// var app = express();
-// app.get('/', function(req, res){
-//     myText = req.query.mytext; //mytext is the name of your input box
-//     res.send('Your Text:' +myText);
-// });
-// console.log(myText);
-
-// const readline = require('readline');
-//
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-//
-// rl.question('What do you think of Node.js? ', (answer) => {
-//     // TODO: Log the answer in a database
-//     console.log(`Thank you for your valuable feedback: ${answer}`);
-//
-// rl.close();
-// });
 
 /////////////// API request for website http://aqicn.org/api/ /////////////////////////////////////////
 var request = require('request');
@@ -136,6 +79,8 @@ function createbargraph(keyword, req, res)
 
             // globalbarHeight = globalaqi / 2;
             // console.log("barheight: " + barHeight);
+
+            //redirect call to refresh page
             res.redirect("http://localhost:3000/");
         }
     });
