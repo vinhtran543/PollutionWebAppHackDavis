d2f171e00781c036a981d868f85a4b8d1d8dc58d
Vinh Tran <vnntran@ucdavis.edu>
2018-07-24 00:32:28 -0700

    Fixed background size and added in aqi table
    Fixed background by changing bottom position of bargraph in css to opx,
    turned off position: absolute, changed index-top css. Did css
    transitions for split right css class to make the widget and table align
    to the right and middle.

--- AirPollution/app.js
+++ AirPollution/app.js
@@ -16,6 +16,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use( express.static( "public" ) ); //allow images to be access from public folder root url instead of full path for EJS
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
